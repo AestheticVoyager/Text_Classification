c49eec090f8bc0b6132945aa53d85607c0dfd9ee
Mahan <55299803+DomirScire@users.noreply.github.com>
2022-02-24 17:31:48 +0330

    Update text_classification.py

--- text_classification.py
+++ text_classification.py
@@ -1,17 +1,12 @@
+# DomirScire
 from __future__ import absolute_import, division, print_function
-
 import tensorflow as tf
 from tensorflow import keras
-
 import numpy as np
 
 imdb = keras.datasets.imdb
 
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
-
-#print(f"Training entries {len(train_data)}. Labels: {len(train_labels)}")
-
-#print(len(train_data[0]), len(train_data[1]))
 
 word_index = imdb.get_word_index()
 
